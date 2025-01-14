a20699531d79e11dafefe113223f4f0534626450
ulic-youthlic <ulic.youthlic@gmail.com>
2025-01-14 19:44:26 +0800

    fix email typo again

--- home/david/configurations/Tytonidae/default.nix
+++ home/david/configurations/Tytonidae/default.nix
@@ -17,7 +17,7 @@
     helix.enable = true;
     gpg.enable = true;
     git = {
-      email = "ulic.youthilc@gmail.com";
+      email = "ulic.youthlic@gmail.com";
       name = "ulic-youthlic";
       signKey = "C6FCBD7F49E1CBBABD6661F7FC02063F04331A95";
     };
