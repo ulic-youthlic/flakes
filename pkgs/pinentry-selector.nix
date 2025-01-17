ca525c5789f0187340910def3fbd0abaf745c684
ulic-youthlic <ulic.youthlic@gmail.com>
2025-01-17 13:20:37 +0800

    fix re-splitting the elements in script

--- pkgs/pinentry-selector.nix
+++ pkgs/pinentry-selector.nix
@@ -8,18 +8,18 @@ pkgs.writeShellApplication {
     if [ -v XDG_SESSION_TYPE ]; then
       case $XDG_SESSION_TYPE in
         tty)
-          pinentry-tty $@;;
+          pinentry-tty "$@";;
         *)
-          pinentry-gnome3 $@;;
+          pinentry-gnome3 "$@";;
       esac
     elif [ -v SSH_CLIENT ] && [ -n "$SSH_CLIENT" ]; then
-      pinentry-tty $@
+      pinentry-tty "$@"
     elif [ -v WAYLAND_DISPLAY ] && [ -n "$WAYLAND_DISPLAY" ]; then
-      pinentry-gnome3 $@
+      pinentry-gnome3 "$@"
     elif [ -v DISPLAY ] && [ -n "$DISPLAY" ]; then
-      pinentry-gnome3 $@
+      pinentry-gnome3 "$@"
     else
-      pinentry-tty $@
+      pinentry-tty "$@"
     fi
   '';
 }
