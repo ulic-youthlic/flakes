c3801762befaaf62974181aea125a91fb5f89c65
ulic-youthlic <ulic.youthlic@gmail.com>
2025-02-08 17:49:45 +0800

    add some xdg mine default applications

--- nixos/modules/gui/niri.nix
+++ nixos/modules/gui/niri.nix
@@ -11,7 +11,41 @@ in
   config = lib.mkIf (cfg.enabled == "niri") {
     environment.systemPackages = with pkgs; [
       bluez
+      cosmic-files
     ];
+    xdg.mime = {
+      enable = true;
+      defaultApplications = {
+        "inode/directory" = [
+          "com.system76.CosmicFiles.desktop"
+        ];
+        "x-scheme-handler/about" = [
+          "firefox.desktop"
+          "chromium-browser.desktop"
+        ];
+        "x-scheme-handler/ftp" = [
+          "firefox.desktop"
+          "chromium-browser.desktop"
+        ];
+        "x-scheme-handler/http" = [
+          "firefox.desktop"
+          "chromium-browser.desktop"
+        ];
+        "x-scheme-handler/https" = [
+          "firefox.desktop"
+          "chromium-browser.desktop"
+        ];
+        "x-scheme-handler/mailto" = [
+          "firefox.desktop"
+          "chromium-browser.desktop"
+        ];
+        "x-scheme-handler/tg" = [ "telegramdesktop.desktop" ];
+        "x-scheme-handler/unknown" = [
+          "firefox.desktop"
+          "chromium-browser.desktop"
+        ];
+      };
+    };
     hardware.bluetooth = {
       enable = true;
     };
