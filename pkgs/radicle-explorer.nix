31e41f14c57647175f39a4383fc1ac7d26b9ecb4
ulic-youthlic <ulic.youthlic@gmail.com>
2025-05-13 10:14:31 +0800

    add config for radicle-explorer

--- pkgs/radicle-explorer.nix
+++ pkgs/radicle-explorer.nix
@@ -1,3 +1,25 @@
 {pkgs, ...}:
 pkgs.radicle-explorer.withConfig {
+  preferredSeeds = [
+    {
+      hostname = "seed.youthlic.fun";
+      port = 443;
+      scheme = "https";
+    }
+    {
+      hostname = "ash.radicle.garden";
+      port = 443;
+      scheme = "https";
+    }
+    {
+      hostname = "seed.radicle.xyz";
+      port = 443;
+      scheme = "https";
+    }
+    {
+      hostname = "seed.radicle.garden";
+      port = 443;
+      scheme = "https";
+    }
+  ];
 }
