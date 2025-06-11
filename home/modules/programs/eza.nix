9b4a5c89c2ceedf07051fef8a1a80b37313a3ca2
ulic-youthlic <ulic.youthlic@gmail.com>
2025-06-11 22:13:26 +0800

    module(fzf,eza): Enable shell integration if possible

--- home/modules/programs/eza.nix
+++ home/modules/programs/eza.nix
@@ -4,15 +4,29 @@
   ...
 }: let
   cfg = config.youthlic.programs.eza;
+  fish-cfg = config.youthlic.programs.fish;
+  bash-cfg = config.youthlic.programs.bash;
+  ion-cfg = config.youthlic.programs.ion;
 in {
   options = {
     youthlic.programs.eza = {
       enable = lib.mkEnableOption "eza";
     };
   };
-  config = {
-    programs.eza = lib.mkIf cfg.enable {
-      enable = true;
-    };
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      programs.eza = {
+        enable = true;
+      };
+    })
+    (lib.mkIf (cfg.enable && fish-cfg.enable) {
+      programs.eza.enableFishIntegration = true;
+    })
+    (lib.mkIf (cfg.enable && bash-cfg.enable) {
+      programs.eza.enableBashIntegration = true;
+    })
+    (lib.mkIf (cfg.enable && ion-cfg.enable) {
+      programs.eza.enableIonIntegration = true;
+    })
+  ];
 }
--- home/modules/programs/fzf.nix
+++ home/modules/programs/fzf.nix
@@ -4,15 +4,25 @@
   ...
 }: let
   cfg = config.youthlic.programs.fzf;
+  fish-cfg = config.youthlic.programs.fish;
+  bash-cfg = config.youthlic.programs.bash;
 in {
   options = {
     youthlic.programs.fzf = {
       enable = lib.mkEnableOption "fzf";
     };
   };
-  config = {
-    programs.fzf = lib.mkIf cfg.enable {
-      enable = true;
-    };
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      programs.fzf = {
+        enable = true;
+      };
+    })
+    (lib.mkIf (cfg.enable && fish-cfg.enable) {
+      programs.fzf.enableFishIntegration = true;
+    })
+    (lib.mkIf (cfg.enable && bash-cfg.enable) {
+      programs.fzf.enableBashIntegration = true;
+    })
+  ];
 }
