7f20dd1e90d9504ec496ba8bc9598f803b8b769b
ulic-youthlic <ulic.youthlic@gmail.com>
2025-06-13 15:06:14 +0800

    module(zioxide): Enable shell integration if possible

--- home/modules/programs/zoxide.nix
+++ home/modules/programs/zoxide.nix
@@ -4,15 +4,25 @@
   ...
 }: let
   cfg = config.youthlic.programs.zoxide;
+  fish-cfg = config.youthlic.programs.fish;
+  bash-cfg = config.youthlic.programs.bash;
 in {
   options = {
     youthlic.programs.zoxide = {
       enable = lib.mkEnableOption "zoxide";
     };
   };
-  config = {
-    programs.zoxide = lib.mkIf cfg.enable {
-      enable = true;
-    };
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      programs.zoxide = {
+        enable = true;
+      };
+    })
+    (lib.mkIf (cfg.enable && fish-cfg.enable) {
+      programs.zoxide.enableFishIntegration = true;
+    })
+    (lib.mkIf (cfg.enable && bash-cfg.enable) {
+      programs.zoxide.enableBashIntegration = true;
+    })
+  ];
 }
