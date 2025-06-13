9209e09b46310839dd9263b91be3ae5a4668ad48
ulic-youthlic <ulic.youthlic@gmail.com>
2025-06-14 01:22:51 +0800

    machine(Tytonidae): Add x86_64_v3 system feature for Tytonidae

--- nixos/configurations/Tytonidae/default.nix
+++ nixos/configurations/Tytonidae/default.nix
@@ -114,6 +114,7 @@
       nvidiaBusId = "PCI:1:0:0";
     };
   };
+  nix = {settings = {system-features = ["gccarch-x86-64-v3"];};};
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
