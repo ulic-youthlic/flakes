be0f119852cde5215e5f000996ac3f9d2d2d4d34
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-02 14:56:13 +0800

    module(stylix): Change of gruvbox-hard theme from ayu_dark

--- nixos/configurations/Tytonidae/stylix.nix
+++ nixos/configurations/Tytonidae/stylix.nix
@@ -7,7 +7,7 @@
     enable = true;
     image = rootPath + "/assets/wallpaper/01.png";
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     cursor = {
       package = pkgs.rose-pine-cursor;
       name = "BreezeX-RosePineDawn-Linux";
