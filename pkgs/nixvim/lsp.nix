56dc7f3bba08e2d8cea3a7580ce291108aea3c15
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-13 06:04:55 +0800

    pkg(nixvim): Add some lsp configuration

--- pkgs/nixvim/lsp.nix
+++ pkgs/nixvim/lsp.nix
@@ -2,10 +2,34 @@
   youthlic.plugins.lspconfig.enable = true;
   lsp = {
     inlayHints.enable = true;
-    servers = {
-      nixd = {
-        enable = true;
-      };
-    };
+    luaConfig.post =
+      #lua
+      ''
+        do
+          vim.diagnostic.config({
+            underline = true,
+            virtual_lines = {
+              current_line = true,
+            },
+            signs = true,
+            severity_sort = true,
+            float = {
+              scope = "cursor",
+              source = "if_many",
+            },
+            jump = {
+              on_jump = function(diagnostic, bufnr)
+                vim.diagnostic.open_float({
+                  scope = "cursor",
+                  severity_sort = true,
+                  source = "if_many",
+                  bufnr = bufnr,
+                })
+              end,
+              wrap = true,
+            },
+          })
+        end
+      '';
   };
 }
