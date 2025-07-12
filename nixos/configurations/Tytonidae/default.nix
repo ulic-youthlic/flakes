dbd049be707ae8579b377aa055d125ed9fe61e2b
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-13 06:04:55 +0800

    Add emacs module to install emacs

--- nixos/configurations/Tytonidae/default.nix
+++ nixos/configurations/Tytonidae/default.nix
@@ -50,6 +50,7 @@
       wshowkeys.enable = true;
       obs.enable = true;
       garage.enable = true;
+      emacs.enable = true;
     };
   };
 
---
+++ nixos/modules/programs/emacs.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.youthlic.programs.emacs;
+in {
+  options = {
+    youthlic.programs.emacs = {
+      enable = lib.mkEnableOption "emacs";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.emacs = {
+      enable = true;
+      install = true;
+      package = with pkgs;
+        (emacsPackagesFor emacs-pgtk).emacsWithPackages (p:
+          with p; [
+            vterm
+            evil
+            gruvbox-theme
+          ]);
+    };
+  };
+}
