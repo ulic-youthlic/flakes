0bae976b088ce22ee1f2141325d5afff243b0b0c
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-13 06:04:55 +0800

    pkgs(nixvim): Add some basic structure

---
+++ pkgs/nixvim/coding.nix
@@ -0,0 +1,16 @@
+{...}: {
+  plugins = {
+    # mini-pairs = {
+    #   enable = true;
+    # };
+    # ts-comments = {
+    #   enable = true;
+    # };
+    # mini-ai = {
+    #   enable = true;
+    # };
+    # lazydev = {
+    #   enable = true;
+    # };
+  };
+}
---
+++ pkgs/nixvim/editor.nix
@@ -0,0 +1,22 @@
+{...}: {
+  plugins = {
+    #   grug-far = {
+    #     enable = true;
+    #   };
+    #   flash = {
+    #     enable = true;
+    #   };
+    #   which-key = {
+    #     enable = true;
+    #   };
+    #   gitsigns = {
+    #     enable = true;
+    #   };
+    #   trouble = {
+    #     enable = true;
+    #   };
+    #   todo-comments = {
+    #     enable = true;
+    #   };
+  };
+}
