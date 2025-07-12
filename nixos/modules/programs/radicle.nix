9ffb8e9b4247e3537b686f2c615671a8a9e18a9b
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-13 06:04:55 +0800

    module(radicle): Fix mismatch of radicle seed server

--- nixos/modules/programs/radicle.nix
+++ nixos/modules/programs/radicle.nix
@@ -34,9 +34,9 @@ in
         settings = {
           publicExplorer = "https://app.radicle.xyz/nodes/$host/$rid$path";
           preferredSeeds = [
-            "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@ash.radicle.garden:8776"
+            "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosa.radicle.xyz:8776"
             "z6MksmpU5b1dS7oaqF2bHXhQi1DWy2hB7Mh9CuN7y1DN6QSz@seed.radicle.xyz:8776"
-            "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@seed.radicle.garden:8776"
+            "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@iris.radicle.xyz:8776"
           ];
           web = {
             bannerUrl = "https://radicle.${config.youthlic.programs.caddy.baseDomain}/images/youthlic-seed-header.png";
