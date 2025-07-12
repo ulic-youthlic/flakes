2300f302af955f67868c6ec690a6b43169932a2a
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-13 06:04:55 +0800

    flake: Sync flake input upstreams

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
     # nixpkgs.follows = "nixos-cosmic/nixpkgs";
 
     lix-module = {
-      url = "git+https://git.lix.systems/lix-project/nixos-module?ref=2.93.2-1";
+      url = "git+https://git.lix.systems/lix-project/nixos-module?ref=release-2.93";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
