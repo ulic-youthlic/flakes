9fe94ef933ec7748ec8c5e0e227a11bdb179090d
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-13 06:04:55 +0800

    gui(niri-hybrid): Fix extra option for regreet

--- nixos/configurations/Tytonidae/specialisation/niri-hybrid.nix
+++ nixos/configurations/Tytonidae/specialisation/niri-hybrid.nix
@@ -3,33 +3,11 @@
   lib,
   ...
 }:
-let
-  extraConfig = ''
-    output "DP-1" {
-      mode "2560x1440@169.900"
-      scale 1.0
-      position x=0 y=0
-      transform "normal"
-      focus-at-startup
-    }
-    output "eDP-1" {
-      mode "2560x1440@165.003"
-      scale 1.5
-      position x=2560 y=0
-      transform "normal"
-    }
-    window-rule {
-      match app-id="apps.regreet"
-      open-on-output "DP-1"
-    }
-  '';
-in
 {
   config.specialisation.niri-hybrid = {
     inheritParentConfig = true;
     configuration = {
       imports = [ inputs.nixos-hardware.nixosModules.common-gpu-nvidia ];
-      youthlic.gui.niri.extraConfig = lib.mkForce extraConfig;
       hardware.nvidia.prime.reverseSync.enable = lib.mkForce false;
     };
   };
