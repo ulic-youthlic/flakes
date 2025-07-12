419edec01f3be200ac9870c1dabbf1d652d08d1a
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-13 06:04:55 +0800

    pkgs(nixvim): Use nixvim to precompile lua to byte code

--- pkgs/nixvim/package.nix
+++ pkgs/nixvim/package.nix
@@ -9,5 +9,21 @@ makeNixvimWithModule {
     imports = with lib; youthlic.loadImports' ./. (filter (name: !hasSuffix "/package.nix" (toString name)));
     enableMan = true;
     plugins.lualine.enable = true;
+    performance = {
+      # combinePlugins = {
+      #   enable = true;
+      #   standalonePlugins = [];
+      # };
+      byteCompileLua = {
+        enable = true;
+        configs = true;
+        initLua = true;
+        luaLib = true;
+        nvimRuntime = true;
+        plugins = true;
+      };
+    };
+    wrapRc = true;
+    luaLoader.enable = true;
   };
 }
