c48460eb3a6f3c81629eadb2367262afd9d74f55
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-13 06:04:55 +0800

    pkg: Enable wayland for spotify

--- pkgs/spotifyx.nix
+++ pkgs/spotifyx.nix
@@ -3,31 +3,33 @@
   unzip,
   zip,
   perl,
-  symlinkJoin,
+  makeWrapper,
   srcs,
 }:
 let
   inherit (srcs) spotx;
-  spotifyx = spotify.overrideAttrs (
-    _final: prev: {
-      nativeBuildInputs = prev.nativeBuildInputs ++ [
-        unzip
-        zip
-        perl
-      ];
-      spotx = spotx.src;
-      postUnpack = ''
-        cp $spotx/spotx.sh ./spotx.sh
-        chmod +x ./spotx.sh
-        patchShebangs --build ./spotx.sh
-      '';
-      postInstall = ''
-        ./spotx.sh -P $out/share/spotify -h
-      '';
-    }
-  );
 in
-symlinkJoin {
-  name = "spotifyx";
-  paths = [ spotifyx ];
-}
+spotify.overrideAttrs (
+  _final: prev: {
+    nativeBuildInputs = prev.nativeBuildInputs ++ [
+      unzip
+      zip
+      perl
+      makeWrapper
+    ];
+    spotx = spotx.src;
+    postUnpack = ''
+      cp $spotx/spotx.sh ./spotx.sh
+      chmod +x ./spotx.sh
+      patchShebangs --build ./spotx.sh
+    '';
+    postInstall = ''
+      ./spotx.sh -P $out/share/spotify -h -p
+    '';
+    postFixup = ''
+      wrapProgram $out/bin/spotify \
+        --set NIXOS_OZONE_WL 1 \
+        --add-flags '--wayland-text-input-version=3'
+    '';
+  }
+)
