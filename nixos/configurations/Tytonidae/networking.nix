d31c5711a31e3ecf8eee83bb892a7a38dedb2ddb
ulic-youthlic <ulic.youthlic@gmail.com>
2025-07-13 06:04:55 +0800

    machine(Tytonidae): Modify network configuration

--- nixos/configurations/Tytonidae/networking.nix
+++ nixos/configurations/Tytonidae/networking.nix
@@ -4,33 +4,19 @@
     wait-online.enable = false;
     networks = {
       "10-eno2" = {
-        dns = [
-          "192.168.31.1"
-        ];
-        address = [
-          "192.168.31.25/24"
-        ];
-        # gateway = [
-        #   "192.168.31.1"
-        # ];
-        routes = [
-          {
-            Gateway = "192.168.31.1";
-          }
-        ];
-        matchConfig.Name = "eno2";
-        linkConfig.RequiredForOnline = "routable";
-        # networkConfig = {
-        #   DHCP = "yes";
-        #   IPv6AcceptRA = true;
-        # };
+        networkConfig = {
+          DHCP = "yes";
+          IPv6AcceptRA = true;
+        };
+        matchConfig = {
+          Path = "pci-0000:00:1f.6";
+        };
       };
     };
   };
 
   networking = {
     networkmanager.enable = false;
-    # useNetworkd = true;
     useDHCP = false;
     nftables = {
       enable = true;
