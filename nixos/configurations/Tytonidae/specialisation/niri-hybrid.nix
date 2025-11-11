52f39f3136b507c2497b1e5853652c2beb8bf4f3
ulic-youthlic <ulic.youthlic@gmail.com>
2025-11-11 18:11:23 +0800

    fix: Fix nvidia setting for niri-hybrid

--- nixos/configurations/Tytonidae/specialisation/niri-hybrid.nix
+++ nixos/configurations/Tytonidae/specialisation/niri-hybrid.nix
@@ -1,13 +1,16 @@
-{
-  inputs,
-  lib,
-  ...
-}: {
+{inputs, ...}: {
   config.specialisation.niri-hybrid = {
     inheritParentConfig = true;
     configuration = {
+      disabledModules = [
+        inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+      ];
       imports = [inputs.nixos-hardware.nixosModules.common-gpu-nvidia];
-      hardware.nvidia.prime.reverseSync.enable = lib.mkForce false;
+      hardware.nvidia.prime = {
+        reverseSync.enable = false;
+        offload.enable = true;
+      };
+      services.xserver.videoDrivers = ["nvidia"];
     };
   };
 }
