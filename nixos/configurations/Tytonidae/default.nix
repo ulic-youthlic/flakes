a8f4ad477fb7105a82c7eb154e6eb265edbdf419
ulic-youthlic <ulic.youthlic@gmail.com>
2026-09-14 09:16:55 +0800

    Readd nvidia related configuration

--- nixos/configurations/Tytonidae/default.nix
+++ nixos/configurations/Tytonidae/default.nix
@@ -11,7 +11,7 @@
     (with inputs.nixos-hardware.nixosModules; [
       common-hidpi
       common-cpu-intel
-      # common-gpu-nvidia
+      common-gpu-nvidia
       common-pc-laptop
       common-pc-laptop-ssd
       asus-battery
