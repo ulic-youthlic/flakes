7b69b518bf98a430ac32652ea016d3799996b3b4
ulic-youthlic <ulic.youthlic@gmail.com>
2026-09-14 09:12:47 +0800

    Remove specialisations of other DEs

--- nixos/configurations/Tytonidae/specialisation/cosmic.nix
+++
@@ -1,8 +0,0 @@
-{ lib, ... }: {
-  config.specialisation.cosmic = {
-    inheritParentConfig = true;
-    configuration = {
-      youthlic.gui.enabled = lib.mkForce "cosmic";
-    };
-  };
-}
--- nixos/configurations/Tytonidae/specialisation/kde.nix
+++
@@ -1,8 +0,0 @@
-{ lib, ... }: {
-  config.specialisation.kde = {
-    inheritParentConfig = true;
-    configuration = {
-      youthlic.gui.enabled = lib.mkForce "kde";
-    };
-  };
-}
